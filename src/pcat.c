fecac45c91cf4a8688f19ad3d3c44e881e24b72a
Alessandro Di Cioccio <kittyteggie@gmail.com>
2026-09-18 20:34:48 -0300

    v0.08
    Huge update for pecho & pcat. Also, I think I finally fixed the Makefile error on my computer, idk, I need to see after pushing

--- src/pcat.c
+++ src/pcat.c
@@ -7,13 +7,15 @@ typedef struct {
   int number_nonblank;
   int show_ends;
   int squeeze_blank;
+  int show_tabs;
+  int show_nonprinting;
 } Options;
 
 void process_stream(FILE *f, const Options *opt, long *line_num) {
   char buf[4096];
 
   if (!opt->number_lines && !opt->number_nonblank && !opt->show_ends &&
-      !opt->squeeze_blank) {
+      !opt->squeeze_blank && !opt->show_tabs && !opt->show_nonprinting) {
     size_t n;
     while ((n = fread(buf, 1, sizeof(buf), f)) > 0) {
       fwrite(buf, 1, n, stdout);
@@ -46,16 +48,41 @@ void process_stream(FILE *f, const Options *opt, long *line_num) {
         printf("%6ld\t", ++(*line_num));
     }
 
-    if (opt->show_ends) {
-      if (len > 0 && line[len - 1] == '\n') {
-        fwrite(line, 1, len - 1, stdout);
-        fputs("$\n", stdout);
+    int has_newline = (len > 0 && line[len - 1] == '\n');
+    ssize_t content_len = has_newline ? len - 1 : len;
+
+    for (ssize_t j = 0; j < content_len; j++) {
+      unsigned char c = (unsigned char)line[j];
+
+      if (c == '\t') {
+        if (opt->show_tabs) {
+          fputs("^I", stdout);
+        } else {
+          fputc(c, stdout);
+        }
+      } else if (opt->show_nonprinting && c != '\n') {
+        if (c >= 128) {
+          fputs("M-", stdout);
+          c -= 128;
+        }
+        if (c < 32) {
+          fputc('^', stdout);
+          fputc(c + 64, stdout);
+        } else if (c == 127) {
+          fputs("^?", stdout);
+        } else {
+          fputc(c, stdout);
+        }
       } else {
-        fwrite(line, 1, len, stdout);
-        fputs("$", stdout);
+        fputc(c, stdout);
       }
-    } else {
-      fwrite(line, 1, len, stdout);
+    }
+
+    if (opt->show_ends) {
+      fputc('$', stdout);
+    }
+    if (has_newline) {
+      fputc('\n', stdout);
     }
   }
   free(line);
@@ -64,12 +91,85 @@ void process_stream(FILE *f, const Options *opt, long *line_num) {
 void print_usage(const char *prog) {
   printf("Usage: %s [OPTION]... [FILE]...\n", prog);
   printf("Concatenate FILE(s) to standard output.\n\n");
-  printf("  -n         number all output lines\n");
-  printf("  -b         number nonempty output lines\n");
-  printf("  -E         display $ at end of each line\n");
-  printf("  -s         squeeze multiple adjacent blank lines\n");
-  printf("  -h, --help display this help and exit\n\n");
+  printf("  -A, --show-all           equivalent to -vET\n");
+  printf("  -b, --number-nonblank    number nonempty output lines\n");
+  printf("  -e                       equivalent to -vE\n");
+  printf("  -E, --show-ends          display $ at end of each line\n");
+  printf("  -n, --number             number all output lines\n");
+  printf("  -s, --squeeze-blank      squeeze multiple adjacent blank lines\n");
+  printf("  -t                       equivalent to -vT\n");
+  printf("  -T, --show-tabs          display TAB characters as ^I\n");
+  printf("  -u                       (ignored)\n");
+  printf("  -v, --show-nonprinting   use ^ and M- notation, except for LFD and "
+         "TAB\n");
+  printf("  -h, --help               display this help and exit\n\n");
   printf("With no FILE, or when FILE is -, read standard input.\n");
+}
+
+int parse_long_opt(const char *arg, Options *opt) {
+  if (strcmp(arg, "--number") == 0)
+    opt->number_lines = 1;
+  else if (strcmp(arg, "--number-nonblank") == 0)
+    opt->number_nonblank = 1;
+  else if (strcmp(arg, "--show-ends") == 0)
+    opt->show_ends = 1;
+  else if (strcmp(arg, "--squeeze-blank") == 0)
+    opt->squeeze_blank = 1;
+  else if (strcmp(arg, "--show-tabs") == 0)
+    opt->show_tabs = 1;
+  else if (strcmp(arg, "--show-nonprinting") == 0)
+    opt->show_nonprinting = 1;
+  else if (strcmp(arg, "--show-all") == 0) {
+    opt->show_nonprinting = 1;
+    opt->show_ends = 1;
+    opt->show_tabs = 1;
+  } else if (strcmp(arg, "--help") == 0)
+    return 2;
+  else
+    return 0;
+  return 1;
+}
+
+int parse_short_flag(char c, Options *opt) {
+  switch (c) {
+  case 'n':
+    opt->number_lines = 1;
+    return 1;
+  case 'b':
+    opt->number_nonblank = 1;
+    return 1;
+  case 'E':
+    opt->show_ends = 1;
+    return 1;
+  case 's':
+    opt->squeeze_blank = 1;
+    return 1;
+  case 'T':
+    opt->show_tabs = 1;
+    return 1;
+  case 'v':
+    opt->show_nonprinting = 1;
+    return 1;
+  case 'u':
+    return 1;
+  case 'A':
+    opt->show_nonprinting = 1;
+    opt->show_ends = 1;
+    opt->show_tabs = 1;
+    return 1;
+  case 'e':
+    opt->show_nonprinting = 1;
+    opt->show_ends = 1;
+    return 1;
+  case 't':
+    opt->show_nonprinting = 1;
+    opt->show_tabs = 1;
+    return 1;
+  case 'h':
+    return 2;
+  default:
+    return 0;
+  }
 }
 
 int main(int argc, char *argv[]) {
@@ -77,21 +177,32 @@ int main(int argc, char *argv[]) {
   int i = 1;
 
   for (; i < argc; i++) {
-    if (argv[i][0] == '-' && argv[i][1] != '\0') {
-      if (strcmp(argv[i], "-n") == 0)
-        opt.number_lines = 1;
-      else if (strcmp(argv[i], "-b") == 0)
-        opt.number_nonblank = 1;
-      else if (strcmp(argv[i], "-E") == 0)
-        opt.show_ends = 1;
-      else if (strcmp(argv[i], "-s") == 0)
-        opt.squeeze_blank = 1;
-      else if (strcmp(argv[i], "-h") == 0 || strcmp(argv[i], "--help") == 0) {
+    if (argv[i][0] == '-' && argv[i][1] == '-' && argv[i][2] != '\0') {
+      int r = parse_long_opt(argv[i], &opt);
+      if (r == 2) {
         print_usage(argv[0]);
         return 0;
-      } else if (strcmp(argv[i], "-") == 0) {
+      } else if (r == 0) {
+        fprintf(stderr, "%s: unknown option: %s\n", argv[0], argv[i]);
+        print_usage(argv[0]);
+        return 1;
+      }
+    } else if (argv[i][0] == '-' && argv[i][1] != '\0') {
+      if (strcmp(argv[i], "-") == 0)
         break;
-      } else {
+
+      int ok = 1;
+      for (char *p = argv[i] + 1; *p != '\0'; p++) {
+        int r = parse_short_flag(*p, &opt);
+        if (r == 2) {
+          print_usage(argv[0]);
+          return 0;
+        } else if (r == 0) {
+          ok = 0;
+          break;
+        }
+      }
+      if (!ok) {
         fprintf(stderr, "%s: unknown option: %s\n", argv[0], argv[i]);
         print_usage(argv[0]);
         return 1;
--- src/pecho.c
+++ src/pecho.c
@@ -1,3 +1,4 @@
+#include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -7,7 +8,7 @@ typedef struct {
   int interpret;
 } Options;
 
-void print_interpreted(const char *s) {
+int print_interpreted(const char *s) {
   for (size_t i = 0; s[i] != '\0'; i++) {
     if (s[i] == '\\' && s[i + 1] != '\0') {
       i++;
@@ -30,9 +31,50 @@ void print_interpreted(const char *s) {
       case 'b':
         fputc('\b', stdout);
         break;
-      case '0':
-        fputc('\0', stdout);
+      case 'f':
+        fputc('\f', stdout);
         break;
+      case 'v':
+        fputc('\v', stdout);
+        break;
+      case 'e':
+        fputc('\033', stdout);
+        break;
+      case 'c':
+        return 1;
+      case '0': {
+        int val = 0, n = 0;
+        i++;
+        while (n < 3 && s[i] >= '0' && s[i] <= '7') {
+          val = val * 8 + (s[i] - '0');
+          i++;
+          n++;
+        }
+        i--;
+        fputc((char)val, stdout);
+        break;
+      }
+      case 'x': {
+        int val = 0, n = 0;
+        i++;
+        while (n < 2 && isxdigit((unsigned char)s[i])) {
+          char c = s[i];
+          int d = (c >= '0' && c <= '9')   ? c - '0'
+                  : (c >= 'a' && c <= 'f') ? c - 'a' + 10
+                                           : c - 'A' + 10;
+          val = val * 16 + d;
+          i++;
+          n++;
+        }
+        i--;
+        if (n == 0) {
+          fputc('\\', stdout);
+          fputc('x', stdout);
+        } else {
+          fputc((char)val, stdout);
+        }
+        break;
+      }
       default:
         fputc('\\', stdout);
         fputc(s[i], stdout);
@@ -42,15 +84,32 @@ void print_interpreted(const char *s) {
       fputc(s[i], stdout);
     }
   }
+  return 0;
 }
 
 void print_usage(const char *prog) {
-  printf("Usage: %s [OPTION]... [STRING]...\n", prog);
+  printf("Usage: %s [SHORT-OPTION]... [STRING]...\n", prog);
+  printf("  or:  %s LONG-OPTION\n", prog);
   printf("Echo the STRING(s) to standard output.\n\n");
   printf("  -n             do not output the trailing newline\n");
-  printf(
-      "  -e             interpret backslash escapes (\\n, \\t, \\\\, etc.)\n");
+  printf("  -e             enable interpretation of backslash escapes\n");
+  printf("  -E             disable interpretation of backslash escapes "
+         "(default)\n");
   printf("  -h, --help     display this help and exit\n");
+  printf("      --version  output version information and exit\n\n");
+  printf("If -e is in effect, the following sequences are recognized:\n\n");
+  printf("  \\\\     backslash\n");
+  printf("  \\a     alert (BEL)\n");
+  printf("  \\b     backspace\n");
+  printf("  \\c     produce no further output\n");
+  printf("  \\e     escape\n");
+  printf("  \\f     form feed\n");
+  printf("  \\n     new line\n");
+  printf("  \\r     carriage return\n");
+  printf("  \\t     horizontal tab\n");
+  printf("  \\v     vertical tab\n");
+  printf("  \\0NNN  byte with octal value NNN (1 to 3 digits)\n");
+  printf("  \\xHH   byte with hexadecimal value HH (1 to 2 digits)\n");
 }
 
 int main(int argc, char *argv[]) {
@@ -58,32 +117,52 @@ int main(int argc, char *argv[]) {
   int i = 1;
 
   for (; i < argc; i++) {
-    if (strcmp(argv[i], "-n") == 0) {
-      opt.no_newline = 1;
-    } else if (strcmp(argv[i], "-e") == 0) {
-      opt.interpret = 1;
-    } else if (strcmp(argv[i], "-h") == 0 || strcmp(argv[i], "--help") == 0) {
+    if (strcmp(argv[i], "--help") == 0) {
       print_usage(argv[0]);
+      return 0;
+    } else if (strcmp(argv[i], "--version") == 0) {
+      printf("echo (custom coreutils clone)\n");
       return 0;
     } else if (strcmp(argv[i], "--") == 0) {
       i++;
       break;
+    } else if (argv[i][0] == '-' && argv[i][1] != '\0') {
+      // Check every char is a valid flag; if not, treat whole arg as string
+      int all_valid = 1;
+      for (char *p = argv[i] + 1; *p != '\0'; p++) {
+        if (*p != 'n' && *p != 'e' && *p != 'E') {
+          all_valid = 0;
+          break;
+        }
+      }
+      if (!all_valid)
+        break;
+
+      for (char *p = argv[i] + 1; *p != '\0'; p++) {
+        if (*p == 'n')
+          opt.no_newline = 1;
+        else if (*p == 'e')
+          opt.interpret = 1;
+        else if (*p == 'E')
+          opt.interpret = 0;
+      }
     } else {
       break;
     }
   }
 
-  for (int j = i; j < argc; j++) {
+  int stop = 0;
+  for (int j = i; j < argc && !stop; j++) {
     if (opt.interpret) {
-      print_interpreted(argv[j]);
+      stop = print_interpreted(argv[j]);
     } else {
       fputs(argv[j], stdout);
     }
-    if (j < argc - 1)
+    if (!stop && j < argc - 1)
       fputc(' ', stdout);
   }
 
-  if (!opt.no_newline)
+  if (!opt.no_newline && !stop)
     fputc('\n', stdout);
 
   return 0;
